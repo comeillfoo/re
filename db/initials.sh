c516d362d696217f15934ad69eef5176cf1e2e2b
come1llf00 <rayveleaveme34@inbox.ru>
2022-05-07 13:17:41 +0300

    feat:9:simplify but with sed

--- db/initials.sh
+++ db/initials.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/bash
-grep -oE "^[A-Z]" $1 > /tmp/1
-grep -oE " [A-Z]" $1 > /tmp/2
-paste --delimiters= /tmp/1 /tmp/2 | sed "s/ /./g" -
-rm -f /tmp/1 /tmp/2
+grep -oE "^[A-Za-z]+ [A-Za-z]+" $1 | sed "s/[a-z]\+ / /g" | grep -oE "[A-Z] [A-Z]"
