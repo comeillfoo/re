f54098e2d278219234513b81cf97b4216def84c4
come1llf00 <rayveleaveme34@inbox.ru>
2022-05-07 12:30:31 +0300

    feat:9:initials

--- db/initials.sh
+++ db/initials.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
-grep -ioP "^[a-z]"\|" \K[a-z]" $1
+grep -oE "^[A-Z]" $1 > /tmp/1
+grep -oE " [A-Z]" $1 > /tmp/2
+paste --delimiters= /tmp/1 /tmp/2 | sed "s/ /./g" -
+rm -f /tmp/1 /tmp/2
